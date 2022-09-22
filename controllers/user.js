f03078656b0c186e806ab9902bf081eb3fcfef17
Michael Briquet <michael.br@free.fr>
2022-09-22 21:21:06 +0200

    ✨ Add check email and password before signup

--- controllers/user.js
+++ controllers/user.js
@@ -5,33 +5,53 @@
 
 const bcrypt = require('bcrypt'); // Need to hash password
 const jwt = require('jsonwebtoken'); // Need to manage session token
+const validator = require('validator');
 const User = require('../models/User');
 
 /**
  * @function signup
  * @description Creating a new user.
  *
- * @param {express.Request}  req               - Request object.
+ * @param {Express.Request}  req               - Request object.
  * @param {object}           req.body          - Request body.
  * @param {string}           req.body.email    - Email of the new user.
  * @param {string}           req.body.password - Password of the new user.
  *
- * @param {express.Response} res               - Response object.
+ * @param {Express.Response} res               - Response object.
  */
-exports.signup = (req, res) => {
-  bcrypt
-    .hash(req.body.password, 15)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch((error) => res.status(500).json({ error }));
+exports.signup = async (req, res) => {
+  if (
+    !validator.isStrongPassword(req.body.password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
     })
-    .catch((error) => res.status(500).json({ error }));
+  ) {
+    return res.status(400).json({
+      message:
+        'Mot de passe requis : 8 caractères minimum, au moins 1 Majuscule, 1 minuscule, 1 caractère spécial, sans espace.',
+    });
+  }
+
+  if (!validator.isEmail(req.body.email)) {
+    return res.status(400).json({ message: 'Adresse email invalide' });
+  }
+
+  const userEmail = await User.findOne({ email: req.body.email });
+  if (userEmail) {
+    return res.status(409).json({ message: "L'adresse email est déjà utilisée" });
+  }
+
+  const hash = await bcrypt.hash(req.body.password, 15);
+  const user = new User({
+    email: req.body.email,
+    password: hash,
+  });
+
+  await user.save().catch((error) => res.status(500).json({ error }));
+  return res.status(201).json({ message: 'Utilisateur créé !' });
 };
 
 /**
